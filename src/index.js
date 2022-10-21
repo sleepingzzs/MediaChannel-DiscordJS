921dd78e53addb5a2d0973c306273b69f20a173e
Abismar <93027509+sleepingzzs@users.noreply.github.com>
2022-10-21 19:26:56 +0530

    i cringed looking at my comments

--- src/index.js
+++ src/index.js
@@ -9,16 +9,15 @@ const { token } = require('../config/token.json');
 
 client.on('ready', (client) => {
     console.log('Bot is online...');
-//Reacts with 👍 and 👎 to the posts
+
     const rating = require('./rating');
     rating(client);
-//Automatically deletes the messages without attachments
+
     const autoDelete = require('./autoDelete');
     autoDelete(client);
-//Creates comment section using Threads
+
     const comments = require('./comments');
     comments(client);
 });
 
 client.login(token);
-//npm run dev
